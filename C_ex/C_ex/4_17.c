1f580ceb7dedd602c65f681e0b1885a972960f67
crink1 <1575460882@qq.com>
2023-04-17 20:28:16 +0800

    练习

---
+++ C_ex/C_ex/4_17.c
@@ -0,0 +1,11 @@
+//void fun(int n) {
+//    int i = 1;
+//    while (i <= n)
+//        i = i * 2;
+//}
+//int** fun(int n) {
+//    int** s = (int**)malloc(n * sizeof(int*));
+//    while (n--)
+//        s[n] = (int*)malloc(n * sizeof(int));
+//    return s;
+//}
