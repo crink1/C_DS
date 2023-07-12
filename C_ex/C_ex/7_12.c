259536cdac1378f01400e9c169cea17ffe0a3ab3
crink1 <1575460882@qq.com>
2023-07-12 23:02:18 +0800

    练习

---
+++ C_ex/C_ex/7_12.c
@@ -0,0 +1,22 @@
+int PartSort3(int* a, int left, int right)
+{
+	int prev = left;
+	int cur = left + 1;
+	int keyi = left;
+	while (cur <= right)
+	{
+		if (a[cur] < a[keyi])
+		{
+			prev++;
+			if (cur != prev)
+			{
+				swap(&a[prev], &a[cur]);
+			}	
+		}
+		cur++;
+	if (left != prev)
+	{
+		swap(&a[prev], &a[keyi]);
+	}
+	return prev;
+}
